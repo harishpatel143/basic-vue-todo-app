6802a60e08e90cb8c0b26ae727f409edd4eef567
Harish Patel <harish.p@simformsolutions.com>
2019-06-21 10:36:28 +0530

    initial comimit

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About HRS PTL</h1>
+    <p>
+      This is all about simple todo application in vue js with vue router and axios
+    </p>
+  </div>
+</template>
+scope
